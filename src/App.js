ce8d104281caf7f0ef3b076b2984748fdb740a2b
megha <meghamini40@gmail.com>
2022-08-16 00:53:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import ImageCard from "./components/imageCard";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <ImageCard />
+    </div>
+  );
+}
+
+export default App;
