ed83a7a58b5dd794e8aa0406170855fffc042155
megha <meghamini40@gmail.com>
2022-08-17 17:33:36 +0530

    update files

--- imgur/src/App.js
+++ imgur/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import ImageCard from "./components/imageCard";
-
+import DropDown from "./components/dropdown"
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <DropDown />
       <ImageCard />
     </div>
   );
