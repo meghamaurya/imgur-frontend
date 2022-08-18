96edc94d552f765d55f91bddbf8ba9c2275efe7f
megha <meghamini40@gmail.com>
2022-08-18 12:09:54 +0530

    adding footer component

---
+++ imgur/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+
+    return (
+        <div className='footer'>
+            <a>about</a>
+            <a>terms</a>
+            <a>rules</a>
+            <a>privacy</a>
+        </div>
+    )
+}
